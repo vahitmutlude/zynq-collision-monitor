3d5c6a88ad8cf5df4f24f6ac7b22833ec67256a4
Vahit Mutlu <vahit.mutlu@study.hs-duesseldorf.de>
2026-04-09 16:38:48 +0200

    refactor: establish top level RTL architecture and extract distance logic into dedicated decision_guard module. wire components and prepare internal signals for future pipeline integration.

--- zynq_collision_monitor/zynq_collision_monitor.srcs/sources_1/new/collision_monitor.vhd
+++ zynq_collision_monitor/zynq_collision_monitor.srcs/sources_1/new/collision_monitor.vhd
@@ -1,6 +1,5 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
 entity collision_monitor is
     Port (
@@ -13,16 +12,27 @@ end collision_monitor;
 
 architecture behavioral of collision_monitor is
 
+component decision_guard is
+    Port(
+        clk : in STD_LOGIC;
+        distance_i  : in STD_LOGIC_VECTOR(15 downto 0);
+        alarm_o     : out STD_LOGIC
+    );
+end component;    
+
+signal filtered_distance : STD_LOGIC_VECTOR(15 downto 0);
+
 begin
-process(clk)
-begin
-    if rising_edge(clk) then
-        if unsigned(distance_i) < 2000 then
-            alarm_o <= '1';
-        else
-            alarm_o <= '0';
-        end if;
-    end if;
-end process;                  
+
+
+
+decisin_guard : decision_guard
+    port map(
+            clk => clk,
+            distance_i => distance_i,
+            alarm_o => alarm_o               
+    
+            );
+          
 
 end behavioral;
